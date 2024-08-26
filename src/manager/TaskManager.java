23dc2a84a76dc3845ab7ff1010c898bb34bcc525
e.kopitsa <e.kopitsa@semrush.com>
2024-08-26 19:08:56 +0200

    TaskManager methods

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -1,20 +1,75 @@
 package manager;
 
+import tasks.Epic;
+import tasks.Subtask;
 import tasks.Task;
 
 import java.util.HashMap;
 
 public class TaskManager {
-    private HashMap<Integer, Task> taskHashMap;
+    private HashMap<Integer, Task> tasks;
+    private HashMap<Integer, Epic> epics;
+    private HashMap<Integer, Subtask> subtasks;
+
     private Integer lastId;
 
     public TaskManager() {
-        taskHashMap = new HashMap<>();
+        tasks = new HashMap<>();
+        epics = new HashMap<>();
+        subtasks = new HashMap<>();
     }
 
     public HashMap<Integer, Task> getTasks() {
-        return taskHashMap;
+        return tasks;
     }
+
+    public HashMap<Integer, Epic> getEpics() {
+        return epics;
+    }
+
+    public HashMap<Integer, Subtask> getSubtasks() {
+        return subtasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.put(++lastId, task);
+    }
+
+    public void addEpic(Epic epic) {
+        epics.put(++lastId, epic);
+    }
+
+    public void addSubtask(Subtask subtask) {
+        subtasks.put(++lastId, subtask);
+    }
+
+    public void removeTask(Integer id) {
+        if (tasks.containsKey(id)) {
+            tasks.remove(id);
+        } else if (epics.containsKey(id)) {
+            epics.remove(id);
+        } else if (subtasks.containsKey(id)) {
+            subtasks.remove(id);
+        }
+    }
+
+    public void updateTask(Integer id, Task task) {
+        if (tasks.containsKey(id)) {
+            tasks.put(id, task);
+        } else if (epics.containsKey(id)) {
+            epics.put(id, (Epic) task);
+        } else if (subtasks.containsKey(id)) {
+            subtasks.put(id, (Subtask) task);
+        }
+    }
+
+    public void deleteAll() {
+        tasks.clear();
+        epics.clear();
+        subtasks.clear();
+    }
+
+
 
 
 
