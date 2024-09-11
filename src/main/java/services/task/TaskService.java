183864609bba0e218965114c35fe9fa42330683c
e.kopitsa <e.kopitsa@semrush.com>
2024-09-11 13:44:21 +0200

    Checkstyle

--- src/main/java/services/task/TaskService.java
+++ src/main/java/services/task/TaskService.java
@@ -13,28 +13,30 @@ public interface TaskService {
     public ArrayList<Subtask> getSubtasks();
 
     public void addTask(Task task);
+
     public void removeTask(Task task);
 
     public void addEpic(Epic epic);
+
     public void removeEpic(Epic epic);
 
     public void addSubtask(Subtask subtask);
+
     public void removeSubtask(Subtask subtask);
 
     public Task getTask(int id);
+
     public Epic getEpic(int id);
+
     public Subtask getSubtask(int id);
 
     public void updateSubtask(Subtask subtask);
+
     public void updateTask(Task task);
+
     public void updateEpic(Epic epic);
 
     public void removeAll();
 
-    /**
-     * Метод должен возвращать последние 10 просмотренных задач.
-     * Просмотром будем считать вызов тех методов, которые получают задачу по идентификатору, —
-     * getTask(int id), getSubtask(int id) и getEpic(int id). От повторных просмотров избавляться не нужно.
-     */
     public List<Task> getHistory();
 }
