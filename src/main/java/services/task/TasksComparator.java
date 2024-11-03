3969d7871de35a5944e391c3c9e3928fb89fad2b
e.kopitsa <e.kopitsa@semrush.com>
2024-11-03 13:57:28 +0100

    TasksComparator update

--- src/main/java/services/task/TasksComparator.java
+++ src/main/java/services/task/TasksComparator.java
@@ -8,6 +8,12 @@ public class TasksComparator implements java.util.Comparator<Task> {
         if (task1.getStartTime() != null && task2.getStartTime() != null) {
             return task1.getStartTime().compareTo(task2.getStartTime());
         }
+        if (task1.getStartTime() != null) {
+            return -1;
+        }
+        if (task2.getStartTime() != null) {
+            return 1;
+        }
 
         return task1.getName().compareTo(task2.getName());
     }
